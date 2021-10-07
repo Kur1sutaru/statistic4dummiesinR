67b43e302805a28b7113029baec3ace2d8fc4ac4
Cristal Villalba <52304532+Kur1sutaru@users.noreply.github.com>
2021-10-07 17:33:54 -0300

    Update Modelo linear generalizado Poisson.R

--- Modelo linear generalizado Poisson.R
+++ Modelo linear generalizado Poisson.R
@@ -1,40 +1,40 @@
-## Curso Modelos de regressão no software R - Módulo 2 ##
+## Curso Modelos de regressÃ£o no software R - MÃ³dulo 2 ##
 
 ## Modelos lineares generalizados ##
 
-## Distribuição de Poisson
-## Utilizadas para variáveis de contagem tais como número de esporos de fungos, 
-## número de habitantes, número de pessoas doentes, número de insetos, número de plantas afetadas, etc
-## Normalmente a variância residual é alta para este tipo de distribuição
+## DistribuiÃ§Ã£o de Poisson
+## Utilizadas para variÃ¡veis de contagem tais como nÃºmero de esporos de fungos, 
+## nÃºmero de habitantes, nÃºmero de pessoas doentes, nÃºmero de insetos, nÃºmero de plantas afetadas, etc
+## Normalmente a variÃ¢ncia residual Ã© alta para este tipo de distribuiÃ§Ã£o
 
 ## Leitura dos dados
-dados<-read.table("C:/Users/hp/OneDrive - Experimental Analytics Corporation/Short course/Modelos de regressão no software R - Módulo 2/dados_contagem.csv",h=T,sep=",")
+dados<-read.table("caminhododiretorio/Modelos de regressÃ£o no software R - MÃ³dulo 2/dados_contagem.csv",h=T,sep=",")
 
 # Sumarizar os dados
 library(dplyr)
 continuous <- select_if(dados, is.numeric)
 summary(continuous)
 
-# Rodando o modelo para analizar o número de infectados nas cidades de São Paulo e Rio de Janeiro 
-# Para este tipo de modelo a distribuição utilizada é Poisson
-glm1 <- glm(formula= Número_de_Infectados ~ Sexo + Cidade, data=dados,
+# Rodando o modelo para analizar o nÃºmero de infectados nas cidades de SÃ£o Paulo e Rio de Janeiro 
+# Para este tipo de modelo a distribuiÃ§Ã£o utilizada Ã© Poisson
+glm1 <- glm(formula= NÃºmero_de_Infectados ~ Sexo + Cidade, data=dados,
             family=poisson)
 summary(glm1)
 
-glm2 <- glm(formula= Número_de_Infectados ~ Sexo, data=dados,
+glm2 <- glm(formula= NÃºmero_de_Infectados ~ Sexo, data=dados,
             family=poisson)
 summary(glm2)
 
-glm3 <- glm(formula= Número_de_Infectados ~ Cidade, data=dados,
+glm3 <- glm(formula= NÃºmero_de_Infectados ~ Cidade, data=dados,
             family=poisson)
 summary(glm3)
 
-# Fazendo predições
-teste<-data.frame(Sexo=c("Homem","Mulher","Homem","Mulher"),Cidade=c("Sâo Paulo","Sâo Paulo","Rio de Janeiro","Rio de Janeiro"), Número_de_Infectados=c(23,35,10,5))
+# Fazendo prediÃ§Ãµes
+teste<-data.frame(Sexo=c("Homem","Mulher","Homem","Mulher"),Cidade=c("SÃ¢o Paulo","SÃ¢o Paulo","Rio de Janeiro","Rio de Janeiro"), NÃºmero_de_Infectados=c(23,35,10,5))
 predict <- round(predict(glm1, teste, type = 'response'),0)
 
-# Calculando a acurácia
-acuracia <- cor(teste$Número_de_Infectados,predict, method = "pearson")
+# Calculando a acurÃ¡cia
+acuracia <- cor(teste$NÃºmero_de_Infectados,predict, method = "pearson")
 acuracia
-acuracia <- cor(teste$Número_de_Infectados,predict, method = "spearman")
+acuracia <- cor(teste$NÃºmero_de_Infectados,predict, method = "spearman")
 acuracia
