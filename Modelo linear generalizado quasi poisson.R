f3eb196fb5e8c4e05fa3982b5a6cc6857e510b60
Cristal Villalba <52304532+Kur1sutaru@users.noreply.github.com>
2021-10-07 17:35:13 -0300

    Update Modelo linear generalizado quasi poisson.R

--- Modelo linear generalizado quasi poisson.R
+++ Modelo linear generalizado quasi poisson.R
@@ -1,14 +1,14 @@
-## Curso Modelos de regress„o no software R - MÛdulo 2 ##
+## Curso Modelos de regress√£o no software R - M√≥dulo 2 ##
 
 ## Modelos lineares generalizados ##
 
-## DistribuiÁ„o de Poisson
-## As vezes o valor da vari‚ncia condicional È muito maior que o valor da mÈdia condicional
-## para caracteres com distribuiÁ„o de Poisson. Neste caso nÛs falamos que os dados de overdispersion.
-## Para estes casos precisamos checar a distribuiÁ„o quasiPoisson
+## Distribui√ß√£o de Poisson
+## As vezes o valor da vari√¢ncia condicional √© muito maior que o valor da m√©dia condicional
+## para caracteres com distribui√ß√£o de Poisson. Neste caso n√≥s falamos que os dados de overdispersion.
+## Para estes casos precisamos checar a distribui√ß√£o quasiPoisson
 
 ## Leitura dos dados
-dados<-read.table("C:/Users/hp/OneDrive - Experimental Analytics Corporation/Short course/Modelos de regress„o no software R - MÛdulo 2/dados_contagem.csv",h=T,sep=",")
+dados<-read.table("caminhododiretorio/Modelos de regress√£o no software R - M√≥dulo 2/dados_contagem.csv",h=T,sep=",")
 
 
 # Sumarizar os dados
@@ -16,27 +16,27 @@ library(dplyr)
 continuous <- select_if(dados, is.numeric)
 summary(continuous)
 
-# Rodando o modelo para analizar o uso de smartphone pela populaÁ„o
-# Para este tipo de modelo a distribuiÁ„o utilizada È binomial
-glm1 <- glm(formula= N˙mero_de_Infectados ~ Sexo + Cidade, data=dados,
+# Rodando o modelo para analizar o uso de smartphone pela popula√ß√£o
+# Para este tipo de modelo a distribui√ß√£o utilizada √© binomial
+glm1 <- glm(formula= N√∫mero_de_Infectados ~ Sexo + Cidade, data=dados,
             family=quasipoisson)
 summary(glm1)
 
-# Se o valor do par‚metro de dispers„o para a distribuiÁ„o quasipoisson
+# Se o valor do par√¢metro de dispers√£o para a distribui√ß√£o quasipoisson
 # for maior que 1 significa que temos overdispersion, e se o valor 
 # for menor do que 1 significa que temos underdispersion
-glm2 <- glm(formula= N˙mero_de_Infectados ~ Sexo, data=dados,
+glm2 <- glm(formula= N√∫mero_de_Infectados ~ Sexo, data=dados,
             family=quasipoisson)
 summary(glm2)
 
-glm3 <- glm(formula= N˙mero_de_Infectados ~ Cidade, data=dados,
+glm3 <- glm(formula= N√∫mero_de_Infectados ~ Cidade, data=dados,
             family=quasipoisson)
 summary(glm3)
 
 # Comparando modelos
 anova(glm1, glm2, glm3, test="Chisq")
 
-# Calcualndo os intervalos de confianÁa baseados em Cameron e Trivedi (2009)
+# Calcualndo os intervalos de confian√ßa baseados em Cameron e Trivedi (2009)
 library(sandwich)
 cov.m1 <- vcovHC(glm2, type="HC0")
 std.err <- sqrt(diag(cov.m1))
@@ -55,15 +55,15 @@ with(glm1, cbind(res.deviance = deviance, df = df.residual,
 with(glm3, cbind(res.deviance = deviance, df = df.residual,
                  p = pchisq(deviance, df.residual, lower.tail=FALSE)))
 
-# Fazendo prediÁıes
-teste<-data.frame(Sexo=c("Homem","Mulher","Homem","Mulher"),Cidade=c("S‚o Paulo","S‚o Paulo","Rio de Janeiro","Rio de Janeiro"), N˙mero_de_Infectados=c(23,35,10,5))
+# Fazendo predi√ß√µes
+teste<-data.frame(Sexo=c("Homem","Mulher","Homem","Mulher"),Cidade=c("S√¢o Paulo","S√¢o Paulo","Rio de Janeiro","Rio de Janeiro"), N√∫mero_de_Infectados=c(23,35,10,5))
 predict <- predict(glm1, teste, type = 'response')
 
-# Calculando a acur·cia
-acuracia <- cor(teste$N˙mero_de_Infectados,predict, method = "pearson")
+# Calculando a acur√°cia
+acuracia <- cor(teste$N√∫mero_de_Infectados,predict, method = "pearson")
 acuracia
-acuracia <- cor(teste$N˙mero_de_Infectados,predict, method = "spearman")
+acuracia <- cor(teste$N√∫mero_de_Infectados,predict, method = "spearman")
 acuracia
 
-# A regress„o de Poisson È calculada via m·xima verossimilhnaÁa.
-# Por isso È necess·rio um grande n˙mero de observaÁıes.
+# A regress√£o de Poisson √© calculada via m√°xima verossimilhna√ßa.
+# Por isso √© necess√°rio um grande n√∫mero de observa√ß√µes.
