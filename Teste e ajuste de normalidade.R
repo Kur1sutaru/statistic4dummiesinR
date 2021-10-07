721771155167babdb29129fe42ab8f4c879acec7
Cristal Villalba <52304532+Kur1sutaru@users.noreply.github.com>
2021-10-07 17:38:52 -0300

    Update Teste e ajuste de normalidade.R

--- Teste e ajuste de normalidade.R
+++ Teste e ajuste de normalidade.R
@@ -1,19 +1,19 @@
-## Curso Modelos de regress„o no software R - MÛdulo 1 ##
+## Curso Modelos de regress√£o no software R - M√≥dulo 1 ##
 ## Teste e ajuste de normalidade ##
 
 ## Leitura dos dados
-dados<-read.table("C:/Users/hp/OneDrive - Experimental Analytics Corporation/Short course/Modelos de regress„o no software R - MÛdulo 1/dados.csv",h=T,sep=",")
+dados<-read.table("caminhododiretorio/Modelos de regress√£o no software R - M√≥dulo 1/dados.csv",h=T,sep=",")
 
-##Teste de homogeneidade e normalidade de vari‚ncia
+##Teste de homogeneidade e normalidade de vari√¢ncia
 ##Calculando assimetria e curtose no R
 ## Tipos de curtose:
 ## C=3 - mesocurtica
-## C>3 - platic˙rtica
-## C<3 - leptoc˙rtica
+## C>3 - platic√∫rtica
+## C<3 - leptoc√∫rtica
 ## Tipos de assimetria
-## AS=0 - MÈdia, moda e mediana coincidem
-## AS>0 - cauda da distribuiÁ„o do lado direito do gr·fico
-## AS<0 - cauda da distribuiÁ„o do lado esquerdo do gr·fico
+## AS=0 - M√©dia, moda e mediana coincidem
+## AS>0 - cauda da distribui√ß√£o do lado direito do gr√°fico
+## AS<0 - cauda da distribui√ß√£o do lado esquerdo do gr√°fico
 require(e1071)
 kurtosis(dados$PROD, na.rm = TRUE,type=3)
 skewness(dados$PROD,type=1)
@@ -29,7 +29,7 @@ shapiro.test(dados$PROD)
 ## Calculando teste de Bartlet
 bartlett.test(dados$PROD~dados$Trat, dados)
 
-##RemoÁ„o de outliers
+##Remo√ß√£o de outliers
 #------------------------------------------------------------------------------------------
 # identificar/remover os pontos discrepantes/influentes
 dados[1,3]<-20
@@ -44,11 +44,11 @@ dados1<-dados[!dados$PROD %in% boxplot.stats(dados$PROD)$out,]
 
 #
 #------------------------------------------------------------------------------------------
-# an·lise com os pontos removidos
+# an√°lise com os pontos removidos
 m1<-aov(PROD~Trat+Rep,dados1)
 layout(1)
 plot(residuals(m1)~Trat+Rep, dados1)
 boxplot.stats(dados1$PROD)$out
 
-## ExercÌcio
-## FaÁa o teste de normalidade e remoÁ„o de outlier para todas as vari·veis
+## Exerc√≠cio
+## Fa√ßa o teste de normalidade e remo√ß√£o de outlier para todas as vari√°veis
